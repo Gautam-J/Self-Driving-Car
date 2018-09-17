1dcf60f1b648287ffc44b72efab00c8f54a11345
Gautam J <34481342+Gautam-J@users.noreply.github.com>
2018-09-17 12:41:38 +0530

    Update grabscreen.py

--- grabscreen.py
+++ grabscreen.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
-import win32gui, win32ui, win32con, win32api
+import win32gui
+import win32ui
+import win32con
+import win32api
+
 
 def grab_screen(region=None):
 
     hwin = win32gui.GetDesktopWindow()
 
     if region:
-            left,top,x2,y2 = region
+            left, top, x2, y2 = region
             width = x2 - left + 1
             height = y2 - top + 1
     else:
@@ -23,10 +27,10 @@ def grab_screen(region=None):
     bmp.CreateCompatibleBitmap(srcdc, width, height)
     memdc.SelectObject(bmp)
     memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
-    
+
     signedIntsArray = bmp.GetBitmapBits(True)
     img = np.fromstring(signedIntsArray, dtype='uint8')
-    img.shape = (height,width,4)
+    img.shape = (height, width, 4)
 
     srcdc.DeleteDC()
     memdc.DeleteDC()
