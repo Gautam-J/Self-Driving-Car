5ac9d6d07ff036f38de9513c7737092a18445b31
Gautam J <gautam.jayapal@gmail.com>
2020-03-28 23:49:05 +0530

    Fully functional project. Car knows the concept of junctions!!!

--- 5 test_model.py
+++ 5 test_model.py
@@ -3,15 +3,14 @@ import cv2
 import time
 from grabscreen import grab_screen
 from getkeys import key_check
-from alexnet import alexnet
+from countdown import CountDown
 from directkeys import PressKey, ReleaseKey, W, A, D
+from tensorflow.keras.models import load_model
 
 '''
 Run this script to test your model in your game in real time. Make sure you
 select your game window after initiating this script.
 '''
-
-MODEL_NAME = 'nfs-final.model'
 
 
 def straight():
@@ -25,7 +24,7 @@ def left():
     print('left')
     PressKey(W)
     PressKey(A)
-    time.sleep(0.09)
+    time.sleep(0.1)
     ReleaseKey(A)
 
 
@@ -33,37 +32,48 @@ def right():
     print('right')
     PressKey(W)
     PressKey(D)
-    time.sleep(0.09)
+    time.sleep(0.1)
     ReleaseKey(D)
 
 
-model = alexnet(width=86, height=56, output=3, channel=1, lr=0.001)
-model.load(MODEL_NAME)
-print('Model Loaded!')
-
-
 def main():
-    for i in list(range(5))[::-1]:
-        print(i + 1)
-        time.sleep(1)
+    CountDown(5)
 
     paused = False
     while True:
 
         if not paused:
-            screen = grab_screen(region=(40, 250, 860, 560))
-            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-            screen = cv2.resize(screen, (86, 56))
+            screen = grab_screen(region=(270, 250, 650, 450))
+            minimap = grab_screen(region=(100, 390, 230, 490))
 
-            prediction = model.predict([screen.reshape(86, 56, 1)])[0]
-            prediction = np.array(prediction) * [0.009, 5, 0.009]
+            screen = cv2.resize(screen, (200, 80))
+            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
+            screen = screen.reshape(1, 80, 200, 3).astype(np.float32)
 
-            if np.argmax(prediction) == 0:
+            minimap = cv2.resize(minimap, (50, 50))
+            minimap = cv2.cvtColor(minimap, cv2.COLOR_BGR2GRAY)
+            minimap = minimap.reshape(1, 50, 50, 1).astype(np.float32)
+
+            screen *= 1 / 255.
+            minimap *= 1 / 255.
+
+            prediction = model.predict([screen, minimap])[0]
+            print(prediction)
+            # prediction = np.array(prediction) * [0.009, 5, 0.009]
+
+            # if np.argmax(prediction) == 0:
+            #     left()
+            # elif np.argmax(prediction) == 1:
+            #     straight()
+            # elif np.argmax(prediction) == 2:
+            #     right()
+
+            if prediction[0] >= THRESHOLD:
                 left()
-            elif np.argmax(prediction) == 1:
-                straight()
-            elif np.argmax(prediction) == 2:
+            elif prediction[2] >= THRESHOLD:
                 right()
+            else:
+                straight()
 
         keys = key_check()
 
@@ -81,4 +91,11 @@ def main():
                 time.sleep(1)
 
 
-main()
+MODEL_PATH = 'models\\1585417347_0.200_0.929\\model.h5'
+THRESHOLD = 0.95
+
+model = load_model(MODEL_PATH)
+print('Model Loaded!')
+
+if __name__ == '__main__':
+    main()
