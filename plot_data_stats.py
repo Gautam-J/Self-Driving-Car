4dbdc630e0a82cd28a0d1994b33a010e632dff25
Gautam J <gautam.jayapal@gmail.com>
2020-03-30 16:56:08 +0530

    plot data_stats

---
+++ plot_data_stats.py
@@ -0,0 +1,43 @@
+import six
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Simple script that plots the data available in data_stats.json
+Reference:
+https://stackoverflow.com/questions/19726663/how-to-save-the-pandas-dataframe-series-data-as-a-figure
+'''
+
+
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax
+
+
+with open('data_stats.json', 'r') as f:
+    data = json.load(f)
+
+df = pd.DataFrame(data['stats'])
+fig = render_mpl_table(df)
+plt.show()
